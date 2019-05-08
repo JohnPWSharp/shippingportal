0d48abb3ffff479a86a72356ce6041e5d888a47c
John Sharp <johns@contentmaster.com>
2019-05-08 13:29:11 +0100

    Update setup-vm.sh

--- server-config/setup-vm.sh
+++ server-config/setup-vm.sh
@@ -19,11 +19,11 @@ sudo cp default /etc/nginx/sites-available
 sudo systemctl restart nginx
 
 # Download shipping portal applicaiton from git
-#git clone https://github.com/<shippingportal-repo> shippingportal
+git clone https://github.com/JohnPWSharp/shippingportal shippingportal
 dotnet new web -n shippingportal
 cd shippingportal
 dotnet build
-dotnet publish -o published -c Release
+dotnet publish -o ./published -c Release
 cd published
 
 sudo mkdir /var/www/shippingportal
