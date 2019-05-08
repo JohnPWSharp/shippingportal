333a48092576b111da6e5bacb5c2ad1fb8377b8b
John Sharp <johns@contentmaster.com>
2019-05-08 13:50:46 +0100

    Add files via upload

--- server-config/setup-vm.sh
+++ server-config/setup-vm.sh
@@ -18,8 +18,8 @@ sudo cp default /etc/nginx/sites-available
 
 sudo systemctl restart nginx
 
-# Download shipping portal applicaiton from git
-git clone https://github.com/JohnPWSharp/shippingportal shippingportal
+# Download shipping portal application from git
+git clone https://github.com/<shippingportal-repo> shippingportal
 dotnet new web -n shippingportal
 cd shippingportal
 dotnet build
